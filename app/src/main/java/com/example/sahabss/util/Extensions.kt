a7f05dda9f5651bd2b6eed24e26e97887bb45b2b
Shahzad Afridi <shahzad@tentwenty.me>
2022-03-19 16:31:49 +0500

    Glide load Image extension function added

---
+++ app/src/main/java/com/example/sahabss/util/Extensions.kt
@@ -0,0 +1,48 @@
+package com.example.sahabss.util
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.example.sahabss.R
+
+
+fun ImageView.load(
+    url: Any?, onLoadingFinished: () -> Unit = {},
+    @DrawableRes placeholder: Int = R.mipmap.ic_launcher,
+    cicular: Boolean = false
+) {
+    val listener = object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onLoadingFinished()
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onLoadingFinished()
+            return false
+        }
+    }
+    Glide.with(this)
+        .load(url)
+        .apply { if (cicular) circleCrop() }
+        .apply(RequestOptions.placeholderOf(placeholder))
+        .listener(listener)
+        .into(this)
+}
