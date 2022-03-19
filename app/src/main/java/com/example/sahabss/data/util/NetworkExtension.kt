3918e5f2f282169c4674695a0c9f30422c962a60
Shahzad Afridi <shahzad@tentwenty.me>
2022-03-19 14:48:54 +0500

    Handling api error extension function modified

--- app/src/main/java/com/example/sahabss/data/util/NetworkExtension.kt
+++ app/src/main/java/com/example/sahabss/data/util/NetworkExtension.kt
@@ -1,7 +1,7 @@
 package com.example.sahabss.data.util
 
 import com.example.sahabss.data.remote.dto.api.ApiErrorDTO
-import com.example.sahabss.data.remote.model.ApiResponse
+import com.example.sahabss.util.UiStateResource
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import retrofit2.HttpException
@@ -10,57 +10,31 @@ import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
-suspend fun <T> makeNetworkRequest(request: suspend () -> ApiResponse<T>): Resource<T> {
-    try {
-        val response = request.invoke()
-        return when {
-            response.error == null && response.data != null -> {
-                Resource.Success(response.data)
-            }
-            response.error != null -> {
-                Resource.Failure(
-                    error = Error.ApiError(
-                        code = response.error.errorCode,
-                        message = response.error.displayErrorMessage,
-                    )
-                )
-            }
-            else -> Resource.Failure(Error.Other)
-        }
-    } catch (t: Throwable) {
-        t.printStackTrace()
-        return t.toNetworkResourceFailure()
-    }
-}
-
-fun Throwable?.toNetworkResourceFailure(): Resource.Failure {
+fun Throwable?.toNetworkResourceFailure(): UiStateResource.Failure {
     return when (this) {
         is ConnectException,
-        is UnknownHostException -> Resource.Failure(Error.NoInternetConnection)
-        is SocketTimeoutException -> Resource.Failure(Error.Timeout)
+        is UnknownHostException -> UiStateResource.Failure(Error.NoInternetConnection)
+        is SocketTimeoutException -> UiStateResource.Failure(Error.Timeout)
         is HttpException -> {
             try {
                 val response = this.response()?.errorBody()
                 val errorResponse = Gson().fromJson(response?.string(), ApiErrorDTO::class.java)
-                var message = "Some error has occurred"
-                errorResponse.error?.let { message = it }
-                errorResponse.message?.let { message = it }
-                Resource.Failure(
+                UiStateResource.Failure(
                     Error.ApiError(
-                        message = message,
+                        message = errorResponse.message,
                         code = this.code()
                     )
                 )
             } catch (e: JsonSyntaxException) {
-                Resource.Failure(
+                UiStateResource.Failure(
                     Error.ApiError(
-                        message = "Some error has occurred",
+                        message = this.message() ?: "",
                         code = this.code()
                     )
                 )
             }
         }
-        is EOFException -> Resource.Failure(Error.NetworkError.InvalidResponse)
-        else -> Resource.Failure(Error.NetworkError.Other)
+        is EOFException -> UiStateResource.Failure(Error.NetworkError.InvalidResponse)
+        else -> UiStateResource.Failure(Error.NetworkError.Other)
     }
 }
