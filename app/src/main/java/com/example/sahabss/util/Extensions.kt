47b27857bf0d307c9c7bb4270a45d3cd340f925b
Shahzad Afridi <shahzad@tentwenty.me>
2022-03-19 16:38:58 +0500

    View extension function added show,invisible,hide and showSnackBar

--- app/src/main/java/com/example/sahabss/util/Extensions.kt
+++ app/src/main/java/com/example/sahabss/util/Extensions.kt
@@ -1,6 +1,7 @@
 package com.example.sahabss.util
 
 import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
@@ -10,6 +11,26 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import com.example.sahabss.R
+import com.google.android.material.snackbar.Snackbar
+
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.invisible(){
+    visibility = View.INVISIBLE
+}
+
+fun View.hide(){
+    visibility = View.GONE
+}
+
+fun View.showSnackBar(message: String?){
+    message?.let {
+        Snackbar.make(this,it,Snackbar.LENGTH_LONG).show()
+    }
+}
 
 
 fun ImageView.load(
